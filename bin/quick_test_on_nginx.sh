633258030f4b2b7273bb5e7823ed3601b121cd34
lyn <zeroleo12345@gmail.com>
2018-06-23 16:30:46 +0800

    [FIXED] 修复nginx转发请求到web的8000端口 (#5)

--- bin/quick_test_on_nginx.sh
+++ bin/quick_test_on_nginx.sh
@@ -1,4 +1,4 @@
 # 镜像: https://hub.docker.com/_/nginx/
 
-# --name 参数来源: docker-compose ps 中的 NAME 
+# --name 参数来源: docker-compose ps 中的 NAME, 当2者都相同时, 会冲突!
 docker run --name vue-nginx -d -p 80:80 nginx:1.13.12-alpine

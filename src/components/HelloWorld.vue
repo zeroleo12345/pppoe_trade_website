a25685679f34be578c2be6093818f7d32b6fcf16
zeroleo12345 <zeroleo12345@163.com>
2018-06-24 14:48:17 +0800

    使用mode: history代替mode:hash (/#/)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -48,7 +48,7 @@ export default {
   methods: {
     select_tariff (id) {
       // console.log(event.target.id)
-      alert(id)
+      alert(this.$route.query.code)
       this.tariff_id = id
     },
     start_pay () {

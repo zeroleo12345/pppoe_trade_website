eeaffeced5d61ded1060e08bc5f63a7bdb7880a4
zeroleo12345 <zeroleo12345@163.com>
2020-01-31 22:52:35 +0800

    [IMPROVED] 删除无用代码

--- src/api/user.js
+++ src/api/user.js
@@ -45,24 +45,7 @@ function postCreateOrder (data = {}) {
   })
 }
 
-/**
- * 忘记密码后通过邮件链接进入页面重置密码，注意与用户正常修改密码做区分
- * @param  {Object} data 参数对象，其结构如下:
- *                         {
- *                           new_password: 'xxxx',    // 用户设置的新密码
- *                           confirm_token: 'xxxx',   // 后端带在url链接中的参数
- *                         }
- */
-function resetPassword (data = {}) {
-  return request({
-    url: '/user/recover/confirm',
-    method: 'PUT',
-    data: data
-  })
-}
-
 export default {
-  resetPassword,
   getTest,
   getUser,
   getResource,

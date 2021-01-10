bb4d4880db0755d3b14c2df1466517de4198315f
zeroleo12345 <zeroleo12345@163.com>
2021-01-10 15:30:43 +0800

    m

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,6 +12,7 @@
       <p class="password">宽带密码： <span :style="qrcode_url !== '' ? 'visibility: hidden': ''">{{ password }}</span></p>
       <p class="status">账号状态： <span :style="{ visibility: qrcode_url !== '' ? 'hidden': 'visible', color: status !== 'working' ? 'red' : 'black' }">{{statusDict[status]}}</span></p>
       <p class="expired_at">到期时间： <span :style="qrcode_url !== '' ? 'visibility: hidden': ''">{{ expired_at }}</span></p>
+      <img class="" :style="{ visibility: qrcode_url === '' ? 'hidden': 'visible'}" :src="qrcode_url"/>
     </div>
 
     <div class="choose_box">

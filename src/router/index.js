b8543e0406e4831ec1fe0d1712fe72658d1cd8f3
zeroleo12345 <zeroleo12345@163.com>
2021-01-24 15:19:47 +0800

    [ADDED] 增加上网教程

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GuideIOS from '@/components/GuideIOS'
+import GuideAndroid from '@/components/GuideAndroid'
 
 Vue.use(Router)
 
@@ -9,7 +11,17 @@ const constantRouterMap = [
     path: '/',
     name: 'HelloWorld',
     component: HelloWorld
-  }
+  },
+  {
+    path: '/ios',
+    name: 'GuideIOS',
+    component: GuideIOS
+  },
+  {
+    path: '/android',
+    name: 'GuideAndroid',
+    component: GuideAndroid
+  },
 ]
 
 export default new Router({
